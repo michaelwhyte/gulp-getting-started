d167628c71f5c0f86843bfe358579ce3d21da31f
Michael Whyte <info@mwhyte.ca>
2017-03-24 20:01:38 -0700

    add sass task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 
+// Intro Hello task...
 gulp.task('hello', function() {
   console.log('Hello, Michael');
+});
+
+gulp.task('sass', function(){
+	return gulp.src('dev/scss/**/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('dev/styles'));
 });
