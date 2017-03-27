6eef58cf603b157c90c999ba727b56956541748d
Michael Whyte <info@mwhyte.ca>
2017-03-26 17:54:31 -0700

    add watch task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,16 @@ gulp.task('hello', function() {
   console.log('Hello, Michael');
 });
 
+// Sass task
 gulp.task('sass', function(){
 	return gulp.src('dev/scss/**/*.scss')
 		.pipe(sourcemaps.init())
 		.pipe(sass().on('error', sass.logError))
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('dev/styles'));
+});
+
+// Watch task
+gulp.task('watch', function(){
+	gulp.watch('dev/scss/**/*.scss', ['sass']);
 });
