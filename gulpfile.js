01b12be8a05329d4f083dd27be968044d6b1507f
Michael Whyte <info@mwhyte.ca>
2017-03-26 19:15:20 -0700

    add browserSync to to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
 
 // Intro Hello task...
 gulp.task('hello', function() {
@@ -13,10 +14,24 @@ gulp.task('sass', function(){
 		.pipe(sourcemaps.init())
 		.pipe(sass().on('error', sass.logError))
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('dev/styles'));
+		.pipe(gulp.dest('dev/styles'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
 });
 
 // Watch task
-gulp.task('watch', function(){
+gulp.task('watch', ['browserSync', 'sass'], function(){
 	gulp.watch('dev/scss/**/*.scss', ['sass']);
+	gulp.watch('dev/*.html', browserSync.reload);
+	gulp.watch('dev/scripts/**/*.js', browserSync.reload);
+});
+
+// BrowserSync Task
+gulp.task('browserSync', function(){
+	browserSync.init({
+		server: {
+			baseDir: 'dev'
+		}
+	})
 });
