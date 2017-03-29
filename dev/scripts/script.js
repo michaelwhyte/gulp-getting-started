d6697db7311e32d8a07b3f6c69615a07c7e19228
Michael Whyte <info@mwhyte.ca>
2017-03-28 20:51:06 -0700

    Add responsive styles

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,14 @@
-// TODO -> Responsive menu code here...
+// Responsive menu code here...
+(function($){
+
+	'use strict';
+
+	const $body = $('body');
+	const $btn = $('.btn-menu');
+
+	$btn.click(function(){
+		$body.toggleClass('show-mobile-menu');
+	});
+		
+	
+})(jQuery);
